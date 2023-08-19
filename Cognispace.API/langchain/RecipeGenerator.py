f7083c605485107d07e912b3e3d265e25e4e4cb5
PiotrZak <piotrzak77@gmail.com>
2023-08-19 22:59:22 +0200

    llama-2

---
+++ Cognispace.API/langchain/RecipeGenerator.py
@@ -0,0 +1,29 @@
+from langchain import PromptTemplate, LLMChain
+from langchain.llms import LlamaCpp
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+# Make sure the model path is correct for your system!
+llm = LlamaCpp(
+    model_path="Llama2/llama7B-2.bin",
+    temperature=0.75,
+    max_tokens=2000,
+    top_p=1,
+    callback_manager=callback_manager,
+    verbose=True,
+    use_mlock=True,
+)
+
+template = """Mood-Based Recipe Generator.
+This agent could generate recipes based on the user's mood or emotions.
+Users could input how they're feeling, and the agent would suggest recipes that match their mood,
+such as comfort foods for a bad day or energizing dishes for a productive day.
+
+{history}
+Human: {human_input}
+Assistant:"""
+
+llm(template)
