c5d85f9edc6a2d16899a387abbc0854e4e320577
Mujeeb Ur rehman <113839359+mujeeb00@users.noreply.github.com>
2023-08-19 13:56:09 +0500

    Feat : init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Home from './features/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
